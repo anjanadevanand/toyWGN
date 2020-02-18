8339ca2208414c9951a22ffde8a630a41a731f9e
Anjana Devanand <a1227855@adelaide.edu.au>
2020-02-18 14:10:47 +1030

    Create data documentation

---
+++ R/obs_data.R
@@ -0,0 +1,88 @@
+#' Annual Maximum Rainfall
+#'
+#' Example of observed annual maximum rainfall statistic calculated from data
+#'     at station no. 23700 from 01 Jan 1889 to 31 Dec 2010 (122 years)
+#'
+#' @source onk.silo.daily.csv
+#' @format Numeric vector of length 122
+#' @examples
+#'   obs_ann_rain_max
+"obs_ann_rain_max"
+
+
+#' Annual Total Rainfall
+#'
+#' Example of observed annual total rainfall statistic calculated from data
+#'     at station no. 23700 from 01 Jan 1889 to 31 Dec 2010 (122 years)
+#'
+#' @source onk.silo.daily.csv
+#' @format Numeric vector of length 122
+#' @examples
+#'   obs_ann_rain_tot
+"obs_ann_rain_tot"
+
+
+#' Correlation of Wet Day Rainfall
+#'
+#' Example of observed wet day rainfall correlations by month calculated from data
+#'     at station no. 23700 from 01 Jan 1889 to 31 Dec 2010
+#'
+#' @source onk.silo.daily.csv
+#' @format Named numeric vector of length 12. The names of the vector are the months
+#'     from Jan to Dec.
+#' @examples
+#'   obs_cor_wetdays_daily
+"obs_cor_wetdays_daily"
+
+
+#' Variance of Wet Day Rainfall
+#'
+#' Example of observed variance of wet day rainfall by month calculated from data
+#'     at station no. 23700 from 01 Jan 1889 to 31 Dec 2010
+#'
+#' @source onk.silo.daily.csv
+#' @format Named numeric vector of length 12. The names of the vector are the months
+#'     from Jan to Dec.
+#' @examples
+#'   obs_var_wetdays_daily
+"obs_var_wetdays_daily"
+
+
+#' Mean Monthly Rainfall
+#'
+#' Example of observed monthly mean rainfall calculated from data
+#'     at station no. 23700 from 01 Jan 1889 to 31 Dec 2010
+#'
+#' @source onk.silo.daily.csv
+#' @format Named numeric vector of length 12. The names of the vector are the months
+#'     from Jan to Dec.
+#' @examples
+#'   obs_mon_rain_mean
+"obs_mon_rain_mean"
+
+
+#' Standard Deviation of Monthly Rainfall
+#'
+#' Example of observed monthly rainfall standard deviations calculated from data
+#'     at station no. 23700 from 01 Jan 1889 to 31 Dec 2010
+#'
+#' @source onk.silo.daily.csv
+#' @format Named numeric vector of length 12. The names of the vector are the months
+#'     from Jan to Dec.
+#' @examples
+#'   obs_mon_rain_sd
+"obs_mon_rain_sd"
+
+
+#' Proportion of Dry Days
+#'
+#' Example of observed proportion of dry (zero-rainfall) days calculated from data
+#'     at station no. 23700 from 01 Jan 1889 to 31 Dec 2010
+#'
+#' @source onk.silo.daily.csv
+#' @format Named numeric vector of length 12. The names of the vector are the months
+#'     from Jan to Dec.
+#' @examples
+#'   obs_pdry
+"obs_pdry"
+
